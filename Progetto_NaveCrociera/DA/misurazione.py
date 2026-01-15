b0effb2e74c34a304197914585d8b379479b8b2f
crepaldimattia <150442472+crepaldimattia@users.noreply.github.com>
2026-01-15 14:01:22 +0200

    Add files via upload

--- Progetto_NaveCrociera/DA/misurazione.py
+++ Progetto_NaveCrociera/DA/misurazione.py
@@ -1,14 +1,18 @@
-
-import random   # Generazione numeri casuali
+import random   # serve per generare numeri casuali
 
 #
 # Funzioni
 #
+
 def leggi_temperatura(N):
+    # genera un valore casuale di temperatura tra 10 e 40
+    # N indica quante cifre decimali voglio
     TEMP = round(random.uniform(10,40), N)
-    return TEMP
-# Simulazione sensore umidità, da 20 a 90 gradi
-# cifre decimali pari a N
+    return TEMP   # restituisce la temperatura
+
+# funzione che simula un sensore di umidità
+# il valore va da 20 a 90
+# N indica il numero di cifre decimali
 def leggi_umidita(N):
-    UMID = round(random.uniform(20,90), N)
-    return UMID
+    UMID = round(random.uniform(20,90), N)  # genera un valore casuale di umidità
+    return UMID   # restituisce l'umidità
